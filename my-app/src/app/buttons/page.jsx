12c865e18b5a78a145b9de45b1caab0a866008d5
max16code <amanzeikedi16@gmail.com>
2025-06-04 16:12:55 +0100

    finalcommit

--- my-app/src/app/buttons/page.jsx
+++ my-app/src/app/buttons/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1>Welcome to My Next.js App</h1>
       <Button text="Click Me" color="blue" />
-      <Button text="Cancel you and me" color="pink" />
+      <Button text="Cancel you and me right now" color="pink" />
     </div>
   );
 }
